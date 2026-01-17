f7c8e886088af72636659d594c1a6bf8dcdc2fa9
sheida-shab <shabankary@gmail.com>
2026-01-17 23:53:07 +0000

    add cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 
 // Create an Express application
 const app = express();
+app.use(cors());
 
 // Define the port where the backend server will run
 const port = 3000;
