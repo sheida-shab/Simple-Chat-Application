8d4a8f03417a98bc50a5833ee9217cbb6da5c922
sheida-shab <shabankary@gmail.com>
2026-01-18 18:28:10 +0000

    update backend and validation

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 // Import Express framework to create a web server
 import express from "express";
+
+// Import CORS middleware to allow requests from other origins (e.g. frontend)
 import cors from "cors";
+
 // Create an Express application
 const app = express();
+
 app.use(cors());
 
 // Define the port where the backend server will run
@@ -13,14 +17,22 @@ app.use(express.json());
 const userMessageArr = [];
 
 //post endpoint
-app.post("/messages",(req,res)=>{
-    userMessageArr.push(req.body);
-    res.send("received");
-})
+
+app.post("/messages", (req, res) => {
+  const { user, text } = req.body;
+  if (!user || !text || typeof user !== "string" || typeof text !== "string") {
+    return res.status(400).json({ error: "Invalid user or text" });
+  }
+  userMessageArr.push(req.body);
+  res.send("received");
+});
 
 //get endpoint
-app.get("/messages",(req,res) => {
-    res.json(userMessageArr)})
+app.get("/messages", (req, res) => {
+  res.json(userMessageArr);
+});
 
 // Start the server and listen for incoming requests
-app.listen(port,()=>{console.error(`Quote server listening on port ${port}`);});
+app.listen(port, () => {
+  console.error(`Chat Server listening on port ${port}`);
+});
